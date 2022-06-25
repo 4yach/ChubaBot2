5a681d0d94950c2e9c0b7db7f7276c00a2ffa3fb
4yach <a.4yach@gmail.com>
2022-06-25 11:23:08 +0300

    Обновил создание промокодов, роли не высвечиваются в виде списка, их нужно вписывать

--- chuba/states/admin/promo/role.py
+++ chuba/states/admin/promo/role.py
@@ -7,14 +7,16 @@ from chuba.bot import Chuba
 
 from chuba.state import (
     button,
-    select,
+    message,
     StateContext,
-    SelectEvent,
+    MessageEvent,
     DiscordMessageState,
     DiscordMessageStateForm)
 
 from chuba.selects import AdminSelects
 from chuba.buttons import AnyUserButtons
+
+from discord.utils import find
 
 
 class AdminPromoRoleForm(DiscordMessageStateForm):
@@ -48,13 +50,20 @@ class AdminPromoRole(DiscordMessageState):
             components=select_layout
         )
 
-    @select(custom_id=AdminSelects.ADMIN_PROMO_ROLE)
+    @message()
     async def select_role(self, ctx: StateContext):
+        guild_id: int = Chuba.config.get_value("server_specific", "id")
+        guild: Guild = await Chuba.fetch_guild(guild_id)
         event = ctx.event
-        if isinstance(event, SelectEvent):
-            with ctx.data() as data:
-                data["Role"] = event.interaction.values[0]
-            await ctx.set("AdminPromoAmount")
+        if isinstance(event, MessageEvent):
+            text = event.message.content
+            role = find(lambda r: r.name == text, guild.roles)
+            if role:
+                with ctx.data() as data:
+                    data["Role"] = text
+                await ctx.set("AdminPromoAmount")
+            else:
+                await event.message.reply(content="Такой роли не существует, создайте ее")
 
     @button(custom_id=AnyUserButtons.ANY_GOBACK)
     async def go_back(self, ctx: StateContext):
